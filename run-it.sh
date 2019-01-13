fb0758fae840dc64e364e56f8d77b9840acda107
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2019-01-13 08:11:41 -0600

    Fix for Cygwin and Git for Windows

--- run-it.sh
+++ run-it.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 set -e
+set -o pipefail
+set -u
 
-docker rm tmp 2>/dev/null || true
-docker build --tag tmp .
-docker run --name tmp --publish 8080:8080/tcp -it tmp
+run=''
+case $(uname) in
+    CYGWIN* | MINGW* ) run=winpty ;;
+esac
+
+$run docker rm tmp 2>/dev/null || true
+$run docker build --tag tmp .
+$run docker run --name tmp --publish 8080:8080/tcp -it tmp
