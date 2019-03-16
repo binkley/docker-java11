38ff43bb5f2bc4d8bd65fcfb13febe27ab47a6f8
Brian Oxley <boxley@thoughtworks.com>
2019-03-16 12:08:54 -0500

    Cleaner

--- run-it.sh
+++ run-it.sh
@@ -4,14 +4,17 @@ set -e
 set -o pipefail
 set -u
 
+name=docker-java11
+version=0-SNAPSHOT
+
 run=''
 case $(uname) in
     CYGWIN* | MINGW* ) run=winpty ;;
 esac
 
-$run docker rm tmp || true
-$run docker build --compress --tag tmp . \
-    --build-arg APP_JAR=build/libs/docker-java11-0-SNAPSHOT.jar \
+$run docker rm $name 2>/dev/null || true
+$run docker build --compress --tag $name . \
+    --build-arg APP_JAR=build/libs/$name-$version.jar \
     --build-arg GRADLE_VERSION=5.2.1 \
     --build-arg JAVA_VERSION=11.0.2
-$run docker run --name tmp --publish 8080:8080/tcp -it tmp
+$run docker run --publish 8080:8080/tcp -it $name
